240e2aa2b4cba8b4874e3023c627e2e24edd8c01
dia218 <sang218@kyonggi.ac.kr>
2025-01-15 13:41:45 +0900

    fix(globals): Fix global.css & Implement PageLayout
    - delete wrong globals.css(2).
    - update globals.css: add shadow.
    - apply globals at stories.
    - implement PageLayout component.

---
+++ src/components/PageLayout/PageLayout.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageLayout from '../../components/PageLayout/PageLayout';
+import { Meta, StoryFn } from '@storybook/react';
+
+export default {
+  title: 'PageLayout',
+  component: PageLayout,
+} as Meta;
+
+const DefaultContent = (
+  <div>
+    <h1>Welcome to the Layout Component</h1>
+    <p>This is a child component inside the Layout.</p>
+  </div>
+);
+
+export const Default: StoryFn = () => <PageLayout>{DefaultContent}</PageLayout>;
