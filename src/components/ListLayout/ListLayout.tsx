43f977d2120ffa6990c95029dd30e83c2bcf0d00
dia218 <sang218@kyonggi.ac.kr>
2025-01-15 18:08:09 +0900

    feat(ListLayout): Implement ListLayout
    - implement product box list module. it can be used at {
     - cart page
     - order-history page
     - product-list page
     - order-management page
     - product-management page
     - etc..
     }
    
    - can change button by PageLayout/ProductBoxOption {
     - default: can view storybook.
     - other: storybook not yet.
     }

---
+++ src/components/ListLayout/ListLayout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ProductBoxBase from '../ProductBox/ProductBoxBase';
+import { ProductDTO } from '../../lib/types/ProductDTO';
+import styles from './ListLayout.module.css';
+
+interface ListLayoutProps {
+  products: ProductDTO[];
+}
+
+const ListLayout: React.FC<ListLayoutProps> = ({ products }) => {
+  return (
+    <div className={styles.listLayout}>
+      {products.map((product) => (
+        <ProductBoxBase
+          key={product.productId}
+          image={product.productImageURL}
+          name={product.productName}
+          description={product.productDescription}
+          price={product.productPrice.toString()} // String으로 변환
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ListLayout;
