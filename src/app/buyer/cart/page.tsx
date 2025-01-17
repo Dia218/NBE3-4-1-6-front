152c7c7022fec1f66617d50105ca07e815095e6a
dia218 <sang218@kyonggi.ac.kr>
2025-01-18 05:55:05 +0900

    feat(CartPage): Implement CartPage View
    - implement cartPage (only view).
    - implement buyerProductService (not tested).
    
    connect #2

--- src/app/buyer/cart/page.tsx
+++ src/app/buyer/cart/page.tsx
@@ -1,11 +1,46 @@
-import OrderRequestComponent from "@/components/OrderRequestLayout/OrderRequestLayout";
+'use client';
 
+import React, { useEffect, useState } from 'react';
+import PageLayout from '../../../components/PageLayout/PageLayout'; // Import PageLayout
+import ListLayout from '../../../components/ListLayout/ListLayout';
+import { getCartList } from '../../../lib/api/buyerProductService';
+import { ProductDTO } from '../../../lib/types/ProductDTO';
+import styles from './CartPage.module.css'; // CSS 모듈 가져오기
+import { PageType } from '@/lib/enum/PageType';
 
-export default function CartPage() {
-    return (
-    <>
-        <OrderRequestComponent totalPrice={1000}/> 
-    </>
-    );
-    // 1000은 임시 데이터로, 실제로는 장바구니에 담긴 상품의 총 가격에 대한 state 를 전달해야 함
-  }
+const CartPage: React.FC = () => {
+  const [products, setProducts] = useState<ProductDTO[]>([]); //백엔드에서 ProductDTO 가져오기
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const fetchedProducts = await getCartList();
+        setProducts(fetchedProducts);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+    
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.pageLayout}>
+        <PageLayout 
+          mainContent={
+            <ListLayout 
+              pageType={PageType.ChangeCart}
+              products={products} 
+            />
+          } 
+        />
+        {/* <div className={styles.OrderRequestComponent}>
+            <OrderRequestComponent totalPrice={1000}/> 
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
