006c069466a25266e58bebd81f78faac2585cf39
dia218 <sang218@kyonggi.ac.kr>
2025-01-17 12:46:31 +0900

    refact(ProductManagement): Use PageLayout

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from './PageLayout.module.css';
 
-const PageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const PageLayout: React.FC<{ mainContent: React.ReactNode }> = ({ mainContent }) => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>Grid&Circle</header>
-      <main className={styles.content}>{children}</main>
+      <main className={styles.content}>{mainContent}</main>
     </div>
   );
 };
