4fc2e390eaf99798951b12461aa8e7f0a85d51fa
LeeKyoWon <kskw20@naver.com>
2025-01-17 15:07:31 +0900

    fix(sellerProductService): change updateProduct() & createProduct()

--- src/lib/api/sellerProductService.ts
+++ src/lib/api/sellerProductService.ts
@@ -1,4 +1,6 @@
 import { ProductDTO } from '../types/ProductDTO';
+import { ErrorDetails } from "../types/ErrorDetails";
+import { ProductRequestDTO } from "../types/ProductRequestDTO";
 
 // Base URL for the backend API
 const BASE_URL = 'http://localhost:8080/seller/products';
@@ -70,5 +72,56 @@ export const deleteProduct = async (productId: number): Promise<void> => {
   });
   if (!response.ok) {
     throw new Error('Failed to delete product');
+  }
+};
+
+export async function submitProduct(
+  formData : ProductRequestDTO
+) {
+  // fetch 전송 (아래의 코드 api로 분리 가능)
+  try {
+      const response = await fetch('http://localhost:8080/seller/products', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData)
+      });
+
+      if(!response.ok) {
+          const errorData: ErrorDetails = await response.json()
+          alert(`오류: ${errorData.message}`);
+      }
+      
+      const data = await response.json();
+      alert('상품 등록이 성공적으로 처리됐습니다.');
+  } catch (error) {
+      alert('상품 등록 처리 중 오류가 발생했습니다.');
+  }
+};
+
+export async function updateProduct(
+  formData : ProductDTO
+) {
+  // fetch 전송 (아래의 코드 api로 분리 가능)
+  try {
+      const response = await fetch(`http://localhost:8080/seller/products/${formData.productId}`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData)
+      });
+
+      if(!response.ok) {
+
+          const errorData: ErrorDetails = await response.json()
+          alert(`오류: ${errorData.message}`);
+      }
+      
+      const data = await response.json();
+      alert('상품 수정이 성공적으로 처리됐습니다.');
+  } catch (error) {
+      alert('상품 등록 처리 중 오류가 발생했습니다.');
   }
 };
